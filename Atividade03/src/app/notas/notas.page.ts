293e3576fdcacde3652c9a173ccf13dc82e9d0e4
mitchellww <warflow01@gmail.com>
2019-09-20 20:20:10 -0300

    comit atividade 3

---
+++ Atividade03/src/app/notas/notas.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Nota } from './nota.model';
+import { NotasService } from './notas.service';
+
+@Component({
+  selector: 'app-notas',
+  templateUrl: './notas.page.html',
+  styleUrls: ['./notas.page.scss'],
+})
+export class NotasPage implements OnInit {
+
+  titulo: string = 'Suas notas'
+  notas:  Nota[];
+
+  constructor(private notasService : NotasService) {}
+  
+  ngOnInit() {
+    this.notas = this.notasService.getNotas(); 
+  }
+
+}
