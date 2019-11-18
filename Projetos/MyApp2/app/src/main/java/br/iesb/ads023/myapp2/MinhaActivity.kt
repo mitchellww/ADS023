e7a00f301884e102925aa3e51eb908d1ebc862b8
mitchellww <warflow01@gmail.com>
2019-11-18 21:25:21 -0200

    Atividade11

---
+++ Projetos/MyApp2/app/src/main/java/br/iesb/ads023/myapp2/MinhaActivity.kt
@@ -0,0 +1,12 @@
+package br.iesb.ads023.myapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MinhaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_minha)
+    }
+}
